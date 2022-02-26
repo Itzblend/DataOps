ddbf702f76991bb1eee0c33ac37e720b71128cb8
Itzblend <huhta.lauri@gmail.com>
2022-02-26 09:19:39 +0000

    Tweaking env variables

--- docker/staging/launch_staging.sh
+++ docker/staging/launch_staging.sh
@@ -2,8 +2,8 @@
 
 vault login -no-print -method userpass username=dataops_bob password=${DATAOPS_BOB_VAULT_PASS}
 
-export DATAOPS_STAGING_DB_USER=$(vault kv get -field=user dataops_staging/database)
-export DATAOPS_STAGING_DB_PASS=$(vault kv get -field=password dataops_staging/database)
+export DATAOPS_STAGING_DB_USER=$(vault kv get -field=user Dataops/database)
+export DATAOPS_STAGING_DB_PASS=$(vault kv get -field=password Dataops/database)
 
 #echo $DATAOPS_STAGING_DB_USER
 #echo $DATAOPS_STAGING_DB_PASS
--- src/cli/main.py
+++ src/cli/main.py
@@ -25,7 +25,7 @@ os.environ["VAULT_TOKEN"] = login_resp["auth"]["client_token"]
 db_secrets = json.loads(os.popen(f"""curl -s \
                         --request GET \
                         -H "X-Vault-Token: {os.environ["VAULT_TOKEN"]}" \
-                        {os.environ["VAULT_ADDR"]}/v1/dataops_staging/data/database""").read())
+                        {os.environ["VAULT_ADDR"]}/v1/Dataops/data/database""").read())
 
 os.environ["DATAOPS_STAGING_DB_PASS"] = db_secrets["data"]["data"]["password"]
 
